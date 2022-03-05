c6066b55a18dd4f6676d3bcc2548e098004824a8
giscafer <giscafer@outlook.com>
2022-03-05 21:27:10 +0800

    fix: 发布时codegen执行yarn命令错误

---
+++ scripts/build-codegen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+    
+# "postcompile": "cd ./app-packages/roothub-codegen && pwd && yarn build",
+# "prebuild": "yarn",
+# 本机 An unexpected error occurred: "Cannot create property '-npm-taobao-org-mirrors' on string '{\"-npm-taobao-org-mirrors\":true}'".
+# 改成shell执行绕过
+
+cd ./app-packages/roothub-codegen 
+pwd
+yarn
+yarn build
